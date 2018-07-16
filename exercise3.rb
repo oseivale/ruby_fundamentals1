bf08aa1840663683a67e097d04892be70d3cc767
Valerie Osei <valerie.osei@gmail.com>
2018-07-16 16:33:18 -0400

    stored value in variable, used 'gets' to request user input. Input printed. Learningdifference between 'gets' and 'gets.chomp'

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,8 @@
 puts "what is your name?"
+
+user_name = gets
+puts "Hello, #{user_name} nice to meet you."
+
+puts "What is your name?"
+user_name= gets.chomp
+puts "Hello, #{user_name} nice to meet you."
