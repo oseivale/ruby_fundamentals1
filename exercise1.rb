9dc385de6e9f90e1c88e987a3ea4a4177abc50ce
Valerie Osei <valerie.osei@gmail.com>
2018-07-16 16:19:48 -0400

    working with strings and intergers in the same string

--- exercise1.rb
+++ exercise1.rb
@@ -16,3 +16,7 @@ puts "Hello\b\b\bGoodbye world"
 puts "Hello\rStart over world"
 
 puts "1. Hello\n2. World"
+
+# Symbols - more reading gound at:http://www.gaurishsharma.com/2013/04/understanding-differences-between-symbols-strings-in-ruby.html, http://www.reactive.io/tips/2009/01/11/the-difference-between-ruby-symbols-and-strings/
+
+# Booleans
---
+++ exercise2.rb
@@ -0,0 +1,6 @@
+puts "The total cost of your meal is $#{55}.\nThe total of your tip is $#{55 * 0.13}."
+puts "The total cost of your meal is $ " + 55.to_s + "\nThe total cost of your tip is $" + (55*0.13).to_s + "."
+
+# String interpolation
+
+puts "The result of 45628 x 7839 is #{45628 * 7839}"
