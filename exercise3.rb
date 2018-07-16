ea5803f65ae411692c4e27dc83f8896484acfc67
Valerie Osei <valerie.osei@gmail.com>
2018-07-16 16:45:48 -0400

    added line to request user input for their age, and printed their age and year of birth

--- exercise3.rb
+++ exercise3.rb
@@ -1,8 +1,12 @@
-puts "what is your name?"
+# puts "what is your name?"
+#
+# user_name = gets
+# puts "Hello, #{user_name} nice to meet you."
+#
+# puts "What is your name?"
+# user_name= gets.chomp
+# puts "Hello, #{user_name} nice to meet you."
 
-user_name = gets
-puts "Hello, #{user_name} nice to meet you."
-
-puts "What is your name?"
-user_name= gets.chomp
-puts "Hello, #{user_name} nice to meet you."
+puts "How old are you?"
+user_age = gets.to_i
+puts "You are #{user_age}, which means that you were born in the year #{2018-user_age}"
