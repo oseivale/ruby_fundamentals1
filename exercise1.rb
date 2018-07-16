8a6c1cc6fb9f1161ef1ef034f8b594143a14e4e4
Valerie Osei <valerie.osei@gmail.com>
2018-07-16 15:16:10 -0400

    working with integers, strings, and some basic operations in Ruby

--- exercise1.rb
+++ exercise1.rb
@@ -3,3 +3,16 @@ puts 2 + 3
 puts 2
 puts 3
 puts 2 + 3
+
+puts "Ada Lovelace lived for #{1852-1815} years."
+
+#Escape sequences
+
+puts "Hello\t\tworld"
+
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\b\b\bGoodbye world"
+
+puts "Hello\rStart over world"
+
+puts "1. Hello\n2. World"
